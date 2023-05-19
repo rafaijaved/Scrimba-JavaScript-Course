e5fe2606f4865fa060ece6242057d0043eddadc0
M. Abdul Rafai Javed <rafaijaved000@gmail.com>
2023-05-19 19:14:32 +0500

    update

--- Chrome-Extension/index.js
+++ Chrome-Extension/index.js
@@ -2,16 +2,34 @@ let myLeads = [];
 const inputEl = document.getElementById("leads-input");
 const inputBtn = document.getElementById("input-btn");
 const leadsList = document.getElementById("leads-list");
+const deleteBtn = document.getElementById("delete-btn");
+
+const myLeadsData = JSON.parse(localStorage.getItem("myLeads"));
+
+if (myLeadsData) {
+  myLeads = myLeadsData;
+  renderLeads();
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+});
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
 });
 
 function renderLeads() {
   let listItems = "";
+
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems += `<li> <a target= '_blank' href='${myLeads[i]}'> ${myLeads[i]} </a> </li>`;
   }
+
   leadsList.innerHTML = listItems;
 }
