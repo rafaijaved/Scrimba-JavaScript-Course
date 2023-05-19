40fa785440e49b14c7484e98d5c60904bf30d90c
M. Abdul Rafai Javed <rafaijaved000@gmail.com>
2023-05-19 21:48:59 +0500

    update

--- Chrome-Extension/index.js
+++ Chrome-Extension/index.js
@@ -3,33 +3,47 @@ const inputEl = document.getElementById("leads-input");
 const inputBtn = document.getElementById("input-btn");
 const leadsList = document.getElementById("leads-list");
 const deleteBtn = document.getElementById("delete-btn");
-
 const myLeadsData = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
 
 if (myLeadsData) {
   myLeads = myLeadsData;
-  renderLeads();
+  render(myLeads);
+}
+
+function render(leads) {
+  let listItems = "";
+
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `<li> <a target= '_blank' href='${leads[i]}'> ${leads[i]} </a> </li>`;
+  }
+
+  leadsList.innerHTML = listItems;
 }
 
 deleteBtn.addEventListener("dblclick", function () {
   localStorage.clear();
   myLeads = [];
-  renderLeads();
+  render(myLeads);
 });
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
+  render(myLeads);
 });
 
-function renderLeads() {
-  let listItems = "";
-
-  for (let i = 0; i < myLeads.length; i++) {
-    listItems += `<li> <a target= '_blank' href='${myLeads[i]}'> ${myLeads[i]} </a> </li>`;
-  }
-
-  leadsList.innerHTML = listItems;
-}
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    function (tabs) {
+      myLeads.push(tabs[0].url);
+      localStorage.setItem("myLeads", JSON.stringify(myLeads));
+      render(myLeads);
+    }
+  );
+});
